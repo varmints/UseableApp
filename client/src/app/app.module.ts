98917fe435054540250e2a24a3263f6e9519dedb
Your Name <you@example.com>
2021-02-14 17:14:20 +0100

    End of client Angular instalation

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FontAwesomeModule, NgbModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
